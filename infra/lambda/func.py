2d6feb96b33a4e22184f3ea21ff4709d90112f14
SuperUsuarioEnUnBlog <pepemolineromoles98@gmail.com>
2025-02-19 16:19:00 +0100

    Update: lambda function

--- infra/lambda/func.py
+++ infra/lambda/func.py
@@ -1,16 +1,32 @@
-import json 
+import json
 import boto3
+from decimal import Decimal
+
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('visitas')
-def lambda_handler(event,context):
-    response = table.get_item(Key={
-        'id':'0'
-    })
-    views = response['Item']['views']
-    views = views + 1 
-    print(views)
-    response = table.put_item(Item={
-        'id':'0',
-        'views':views
-    })
-    return views 
+table_visitas = dynamodb.Table('visitas')
+table_direcciones = dynamodb.Table('direcciones')
+
+def decimal_to_int(obj):
+    if isinstance(obj, Decimal):
+        return int(obj)
+    raise TypeError
+
+def lambda_handler(event, context):
+
+    try:
+        body = json.loads(event.get('body', '{}'))
+        ip_address = body.get('ip', 'Desconocida')
+
+        response = table_visitas.get_item(Key={'id': '0'})
+        views = response.get('Item', {}).get('views', 0) + 1
+
+        table_visitas.put_item(Item={'id': '0', 'views': views})
+
+        table_direcciones.put_item(Item={'ip': ip_address})
+
+        return views
+        
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return "Ocurrió un error al procesar la solicitud"
